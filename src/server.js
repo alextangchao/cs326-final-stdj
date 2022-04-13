0ea2ef51f50d984607475ad314f7002b752addcd
Silvennnn <xumingdeng@umass.edu>
2022-04-13 00:49:32 -0400

    Create Fake Review

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import { readFile, writeFile } from 'fs/promises';
 import { faker } from '@faker-js/faker';
 
+const dining_hall = ['hampshire', 'franklin', 'berkshire', 'worcester']
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(logger('dev'));
@@ -15,13 +16,40 @@ const fake_user = {
     pfp_id: faker.datatype.uuid()
 }
 
-const fake_review = {
+const fake_review_1 = {
     id: faker.datatype.uuid(),
     user_id: 0,
+    user_name: faker.name.findName(),
     review_text: faker.lorem.paragraph(),
+    review_num: faker.random.number(100),
     review_img_id: faker.datatype.uuid(),
-    created_date: faker.date.past()
+    created_date: faker.date.past(),
+    location:  faker.random.arrayElement(dining_hall)
 }
+
+const fake_review_2 = {
+    id: faker.datatype.uuid(),
+    user_id: 0,
+    user_name: faker.name.findName(),
+    review_text: faker.lorem.paragraph(),
+    review_num: faker.random.number(100),
+    review_img_id: faker.datatype.uuid(),
+    created_date: faker.date.past(),
+    location:  faker.random.arrayElement(dining_hall)
+}
+
+const fake_review_3 = {
+    id: faker.datatype.uuid(),
+    user_id: 0,
+    user_name: faker.name.findName(),
+    review_text: faker.lorem.paragraph(),
+    review_num: faker.random.number(100),
+    review_img_id: faker.datatype.uuid(),
+    created_date: faker.date.past(),
+    location:  faker.random.arrayElement(dining_hall)
+}
+
+const fake_review_list = [fake_review_1, fake_review_2, fake_review_3]
 
 app.get('/', async (request, response) => {
     response.send('Hello World!');
@@ -37,12 +65,20 @@ app.delete('/user/delete', async (request, response) => {
 });
 
 // REVIEWS
+app.post('/review/create', async (request, response) => {
+    response.status(200).json(fake_review_1);
+});
+
+app.post('/review/location', async (request, response) => {
+    response.stutas(200).json(fake_review_list);
+});
+
 app.put('/review/update', async (request, response) => {
-    response.status(200).json(fake_review);
+    response.status(200).json(fake_review_1);
 });
 
 app.delete('/review/delete', async (request, response) => {
-    response.status(200).json(fake_review);
+    response.status(200).json(fake_review_1);
 });
 
 app.listen(port, () => {
