a91b852e69069d06230a625bd973d2e988f54550
Silvennnn <xumingdeng@umass.edu>
2022-04-13 14:26:15 -0400

    Update review_crud
    Added readReview, getReviewsByLocaiton method

--- src/client/js/review_crud.js
+++ src/client/js/review_crud.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 export async function updateReview(review) {
   const response = await fetch(`/review/update`, {
     method: 'PUT',
@@ -32,4 +34,20 @@ export async function createReview(review) {
   });
   const data = await response.json();
   return data;
+}
+
+export async function readReview(review_id) {
+  const response = await fetch(`/review?id=${review_id}`, {
+    method: 'GET'
+  });
+  const data = await response.json();
+  return data;
+}
+
+export async function getReviewsByLocation(review_location) {
+  const response = await fetch(`/review/location?id=${review_location}`,{
+    method: 'GET'
+  });
+    const data = await response.json();
+    return data;
 }
