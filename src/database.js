18488f724dd80e2dbaa0ffb4ebec652923147f38
alextangchao <alextangchaolong@gmail.com>
2022-04-28 00:40:24 -0400

    [WIP] add post and get image crud function

--- src/database.js
+++ src/database.js
@@ -1,12 +1,47 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 import 'dotenv/config';
 import crypto from 'crypto';
 
-const username = process.env['DB_USERNAME'];
-const pwd = encodeURIComponent(process.env['PWD']);
 
-const uri = `mongodb+srv://${username}:${pwd}@cluster0.ycngz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+export class DB_CRUD {
+    constructor() {}
+
+    async connect(db_url, db_name) {
+        try {
+            this.client = await MongoClient.connect(db_url, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverApi: ServerApiVersion.v1
+            });
+            this.db = this.client.db(db_name);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async addUserToDB(user) {
+        user.password = crypto_hash(String(user.password));
+        await this.db.collection("user").insertOne(user);
+    }
+
+    //image
+    async addImage(image) {
+        const result = await this.db.collection("image").insertOne({data: image});
+        return result.insertedId
+    }
+
+    async getImage(id) {
+        return await this.db.collection("image").find({_id: id});
+    }
+
+    async deleteImage(id) {
+        return await this.db.collection("image").deleteOne({_id: id});
+    }
+}
+
+export function crypto_hash(password) {
+    return crypto.createHash('sha256').update(password).digest('hex');
+}
 
 async function main() {
     try {
@@ -25,19 +60,3 @@ async function listDatabases(client) {
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
-
-export function crypto_hash(password) {
-    return crypto.createHash('sha256').update(password).digest('hex');
-}
-
-export async function addUserToDB(user) {
-    try {
-        await client.connect();
-        user.password = crypto_hash(String(user.password));
-        await client.db("foodandumass").collection("user").insertOne(user);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
--- src/server.js
+++ src/server.js
@@ -1,31 +1,40 @@
 import 'dotenv/config';
-import express, { response } from 'express';
+import express, {response} from 'express';
 import logger from 'morgan';
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
 import passport from 'passport';
-import { auth_setup } from './auth.js';
-import { addUserToDB } from './database.js';
+import multer from 'multer';
+import {auth_setup} from './auth.js';
+import {DB_CRUD} from './database.js';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const username = process.env['DB_USERNAME'];
+const pwd = encodeURIComponent(process.env['PWD']);
+const DB_URL = `mongodb+srv://${username}:${pwd}@cluster0.ycngz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const DB_NAME = "foodandumass";
+
+const db_crud = new DB_CRUD();
+await db_crud.connect(DB_URL, DB_NAME);
+
 const dining_hall = ['hampshire', 'franklin', 'berkshire', 'worcester']
 const app = express();
 const port = process.env.PORT || 3000;
+const upload = multer({limits: {fileSize: 1.6e7}});
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use('/', express.static(join(__dirname, 'client')));
 
 const fake_user = {
-    id: 0,
-    name: faker.name.findName(),
-    pfp_id: faker.datatype.uuid(),
-    password: faker.animal.type()
+    id: 0, name: faker.name.findName(), pfp_id: faker.datatype.uuid(), password: faker.animal.type()
 };
 
 const fake_review_1 = {
@@ -35,7 +44,7 @@ const fake_review_1 = {
     review_text: faker.lorem.paragraph(),
     review_num: faker.datatype.number(100),
     review_img_id: faker.datatype.uuid(),
-    rating: faker.datatype.number({ min: 1, max: 5 }),
+    rating: faker.datatype.number({min: 1, max: 5}),
     created_date: faker.date.past(),
     location: faker.random.arrayElement(dining_hall)
 }
@@ -47,7 +56,7 @@ const fake_review_2 = {
     review_text: faker.lorem.paragraph(),
     review_num: faker.datatype.number(100),
     review_img_id: faker.datatype.uuid(),
-    rating: faker.datatype.number({ min: 1, max: 5 }),
+    rating: faker.datatype.number({min: 1, max: 5}),
     created_date: faker.date.past(),
     location: faker.random.arrayElement(dining_hall)
 }
@@ -59,7 +68,7 @@ const fake_review_3 = {
     review_text: faker.lorem.paragraph(),
     review_num: faker.datatype.number(100),
     review_img_id: faker.datatype.uuid(),
-    rating: faker.datatype.number({ min: 1, max: 5 }),
+    rating: faker.datatype.number({min: 1, max: 5}),
     created_date: faker.date.past(),
     location: faker.random.arrayElement(dining_hall)
 }
@@ -106,12 +115,12 @@ app.post('/user/login', passport.authenticate('local', {
 app.post('/user/register', async (request, response) => {
     const options = request.body;
     if ('username' in options && 'password' in options) {
-        const user = { username: options.username, password: options.password };
+        const user = {username: options.username, password: options.password};
         // to do handle images
-        await addUserToDB(user);
+        await db_crud.addUserToDB(user);
         response.status(200).json(user);
     } else {
-        response.status(400).json({ error: "Bad Requset: Missing params" });
+        response.status(400).json({error: "Bad Requset: Missing params"});
     }
 });
 
@@ -138,21 +147,30 @@ app.delete('/review/delete', async (request, response) => {
 });
 
 // IMAGE
-app.post('/image/create', async function (req, response) {
-    response.status(200).json(fake_image_id);
+app.post('/image/create', upload.single('image'), async function (req, response) {
+    console.log("image create", req.file);
+    response.status(200).json({image: req.file});
+    // const image = req.body.image
+    // if (image === undefined || image === null) {
+    //     response.status(400).json({error: "Bad Requset: Missing params"});
+    // } else {
+    //     const id = await db_crud.addImage(image);
+    //     response.status(200).json({id: id});
+    // }
 });
 
 app.get('/image', async function (req, response) {
-    response.sendFile(__dirname + FILE_PATH);
+    const id = req.query.id;
+    if (id === undefined || id === null) {
+        response.status(400).json({error: "Bad Requset: Missing params"});
+    } else {
+        const image = await db_crud.getImage(id);
+        response.status(200).json({id: id});
+    }
 });
 
 app.delete('/image/delete', async function (req, response) {
     response.status(200).json(fake_image_id);
-});
-
-app.get('/test/var', async function (req, response) {
-    const data = process.env.TEST_VAR;
-    response.status(200).json({ data: data });
 });
 
 app.listen(port, () => {
